1b08950a9bcf672c2112ad5a4c995c0851126026
Samip Nepal <Samip Nepal>
2016-08-07 10:33:19 +0530

    first commit

---
+++ src/main/java/com/restaurant/controller/DefaultController.java
@@ -0,0 +1,27 @@
+package com.restaurant.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.restaurant.bean.UserBean;
+
+@Controller
+@RequestMapping("/hello")
+public class DefaultController {
+
+	@RequestMapping(method=RequestMethod.GET)
+	public @ResponseBody UserBean getUser(){
+		System.out.println("Inside Controller");
+		
+		UserBean userBean=new UserBean();
+		userBean.setFirstName("Yadab");
+		userBean.setLastName("Nepal");
+		userBean.setEmail("samipnepal@gmail.com");
+		userBean.setId("YA001");
+		userBean.setUsername("samipnepal");
+		userBean.setPassword("nepal");
+		return userBean;
+	}
+}
