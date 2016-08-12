a858bd8364a2a79e881b67145ad7ae875d778fe5
Samip Nepal <Samip Nepal>
2016-08-12 09:23:46 +0530

    Service Impl added

---
+++ src/main/java/com/restaurant/service/impl/MenuServiceImpl.java
@@ -0,0 +1,35 @@
+package com.restaurant.service.impl;
+
+import java.util.List;
+
+import com.restaurant.bean.MenuBean;
+import com.restaurant.service.MenuService;
+
+public class MenuServiceImpl implements MenuService {
+
+	public List<MenuBean> getAllMenu() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public MenuBean getMenu(String itemName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String addMenu(MenuBean menuBean) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String removeMenu(String itemName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String updateMenu(MenuBean menuBean) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
